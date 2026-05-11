ef2ddc08f7ff815b162defdf2c3096b0d4c8ecca
Данил Чугунов <136282723+amoremea@users.noreply.github.com>
2026-05-11 23:30:18 +0500

    fix: fixes

--- server/server.js
+++ server/server.js
@@ -64,7 +64,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
